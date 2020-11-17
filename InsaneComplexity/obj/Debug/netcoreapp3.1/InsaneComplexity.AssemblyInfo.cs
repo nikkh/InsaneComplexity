ee4213a42bac9e28ed131e4eb3b9f57084374c8b
nhill <nhill@MININT-CU56C8E>
2020-11-17 17:38:40 +0000

    Dockerise

--- InsaneComplexity/obj/Debug/netcoreapp3.1/InsaneComplexity.AssemblyInfo.cs
+++ InsaneComplexity/obj/Debug/netcoreapp3.1/InsaneComplexity.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("8da79d72-8284-41b4-aa95-83614f4c36d3")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("InsaneComplexity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
